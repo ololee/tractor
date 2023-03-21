0654d5ae2201fea1de22c7d696ea14a53a5ce501
LAPTOP-D75Q22DA\ololee <ololeecn@gmail.com>
2023-03-21 23:43:15 +0800

    集成bugly

--- app/src/main/java/cn/ololee/usbserialassistant/App.java
+++ app/src/main/java/cn/ololee/usbserialassistant/App.java
@@ -3,6 +3,7 @@ package cn.ololee.usbserialassistant;
 import android.app.Application;
 import com.baidu.mapapi.CoordType;
 import com.baidu.mapapi.SDKInitializer;
+import com.tencent.bugly.crashreport.CrashReport;
 import com.tencent.mmkv.MMKV;
 
 public class App extends Application {
@@ -11,5 +12,6 @@ public class App extends Application {
     MMKV.initialize(this);
     SDKInitializer.initialize(this);
     SDKInitializer.setCoordType(CoordType.BD09LL);
+    CrashReport.initCrashReport(getApplicationContext(), "注册时申请的APPID", false);
   }
 }
